fbbb578b8e7ecd636c8dd5c8483ef67c43a8f1d1
kikoken831 <kennyhu831@gmail.com>
2023-05-13 19:30:46 +0800

    Adding static path

--- server/server.js
+++ server/server.js
@@ -1,17 +1,18 @@
 const express = require('express')
 const bodyParser = require("body-parser")
 const cors = require("cors")
+const path = require("path");
 const app = express()
 const PORT = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(bodyParser.urlencoded({extended: true}));
 let jsonParser = bodyParser.json();
-
+app.use(express.static(path.join(__dirname, "../client/build")));
 app.get("/api", (req,res) => {
     res.json({"users":["abc","cde","efg"]})
 })
 
 app.listen(PORT,()=>{
-    console.log("Server started on port 5000")
+    console.log(`Server is running on port ${PORT}`)
 })
