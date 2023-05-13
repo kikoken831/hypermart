9b7025e65aec099025b7621cbd07e7b599ab95cf
kikoken831 <kennyhu831@gmail.com>
2023-05-13 19:26:42 +0800

    Adding port

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ const express = require('express')
 const bodyParser = require("body-parser")
 const cors = require("cors")
 const app = express()
+const PORT = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(bodyParser.urlencoded({extended: true}));
@@ -11,6 +12,6 @@ app.get("/api", (req,res) => {
     res.json({"users":["abc","cde","efg"]})
 })
 
-app.listen(5000,()=>{
+app.listen(PORT,()=>{
     console.log("Server started on port 5000")
 })
